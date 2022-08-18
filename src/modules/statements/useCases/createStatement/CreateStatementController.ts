5dd083f215c2fb933fb9b4d994d522b80047c853
Paulo Victor Rios <victorios.ba@gmail.com>
2022-08-18 09:59:04 -0300

    export enum type

--- src/modules/statements/useCases/createStatement/CreateStatementController.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.ts
@@ -1,12 +1,8 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { OperationType } from '../../entities/Statement';
 
 import { CreateStatementUseCase } from './CreateStatementUseCase';
-
-enum OperationType {
-  DEPOSIT = 'deposit',
-  WITHDRAW = 'withdraw',
-}
 
 export class CreateStatementController {
   async execute(request: Request, response: Response) {
