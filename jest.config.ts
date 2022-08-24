29cc2507c298b89ac6a62bd2daf0c3d9daf8b3d8
Paulo Victor Rios <victorios.ba@gmail.com>
2022-08-24 06:19:50 -0300

    add coverage

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,11 @@ const config: Config.InitialOptions = {
   testEnvironment: "node",
   verbose: true,
   setupFiles: ["<rootDir>/test/setup-tests.ts"],
-}
+  bail: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/modules/**/useCases/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text-summary", "lcov"],
+};
 
 export default config
